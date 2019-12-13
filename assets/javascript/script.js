a3bb41bf4e2960d94078d51449c69965e4c2a173
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-13 18:38:35 -0500

    moved the variable city inside the event listener

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,10 @@
-var city = document.getElementById("search-input").value;
+
 
 $("#search-button").on("click", function() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=5f20ec22761478f699827153c2dae50d";
+  var city = document.getElementById("search-input").value;
+  alert(city);
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5f20ec22761478f699827153c2dae50d";
 
-    console.log(city);
     $.ajax({
         url: queryURL,
         method: "GET"
