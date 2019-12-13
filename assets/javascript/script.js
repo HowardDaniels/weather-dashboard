f007714a47b6d65d87aa10bca7a50eb7e8da9dea
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-13 14:07:13 -0500

    added AJAX and then functions to the JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,4 +3,13 @@ var city = $("#search-input").val();
 $("#search-button").on("click", function() {
     var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      .then(function(response) {
+          
+      });
+
 });
