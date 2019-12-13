003d8f6b76d4852f5c464838e67821f901cf8844
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-13 18:27:52 -0500

    changed query URL to include API key

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,9 @@
-var city = $("#search-input").val();
+var city = document.getElementById("search-input").value;
 
 $("#search-button").on("click", function() {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=5f20ec22761478f699827153c2dae50d";
 
+    console.log(city);
     $.ajax({
         url: queryURL,
         method: "GET"
