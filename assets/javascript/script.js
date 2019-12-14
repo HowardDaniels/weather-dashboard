9840d07eb4c487aed2702865729b4c1d3d068582
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-14 10:05:45 -0500

    added moment.js as well as the city and date to the text after the search button is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@
 
 $("#search-button").on("click", function() {
   var city = document.getElementById("search-input").value;
-  alert(city);
+  console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5f20ec22761478f699827153c2dae50d";
 
     $.ajax({
@@ -12,6 +12,8 @@ $("#search-button").on("click", function() {
 
       .then(function(response) {
           console.log(response);
+          $("#chosen-city").text(response.name);
+          $("#date").text(new Date());
       });
 
 });
