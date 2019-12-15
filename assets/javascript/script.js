a41cc70a2db6af997ad3f52792744d98d9cdc2c1
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-15 18:33:17 -0500

    added uv index with uv index API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,8 @@ $("#search-button").on("click", function() {
   var city = document.getElementById("search-input").value;
   console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5f20ec22761478f699827153c2dae50d";
+    
+
 
     $.ajax({
         url: queryURL,
@@ -22,6 +24,21 @@ $("#search-button").on("click", function() {
           $("#temperature").text(Math.round(response.main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
           $("#humidity").text(response.main.humidity + "%");
           $("#wind-speed").text(Math.round(response.wind.speed * 1.609) + " kph/" + Math.round(response.wind.speed) + " mph");
-      });
+          var lat = response.coord.lat;
+          var lon = response.coord.lon;
+
+      var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=5f20ec22761478f699827153c2dae50d&lat=" + lat + "&lon=" + lon;
+
+      $.ajax({
+        url: queryURL2,
+        method: "GET"
+      })
+
+      .then(function(response) {
+        console.log(response);
+        $("#uv-index").text(response.value);
+    });
+
+  });
 
 });
