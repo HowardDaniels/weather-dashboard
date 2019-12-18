ab49a5b7271d67ef6c99cd625a8f57ce9c5325c2
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-18 16:10:49 -0500

    made the id of two-word cities have a hyphen instead of a space in it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,22 @@
-
+var cities = 0;
 
 $("#search-button").on("click", function() {
   var city = document.getElementById("search-input").value;
   console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5f20ec22761478f699827153c2dae50d";
-    
-    $("#search-containers").prepend("<button class='citycontainer' id =" + city + "></button>");
-    $("#" + city).text(city);
+  
+  /*
+    if (cities < 12){
+      cities += 1; */
+      var citystring = city.replace(" ", "-");
+
+    $("#search-containers").prepend("<button class='citycontainer' id =" + citystring + "></button>");
+    $("#" + citystring).text(city);
+  //  } /*
+  //  else {
+      
+      console.log(document.getElementById("search-containers").children);
+    //} 
 
 function api(){
     $.ajax({
