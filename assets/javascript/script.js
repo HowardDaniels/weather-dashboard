73162e2f09de537535ffa714749559cddd70fcd8
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-19 17:13:19 -0500

    last search item appears upon reloading the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,13 +2,95 @@ $(document).ready(function(){
 var cityButtonsUnparsed = localStorage.getItem("cityArray");
 var cityButtonsParsed = cityButtonsUnparsed ? cityButtonsUnparsed.split(',') : [];
 console.log(cityButtonsParsed[1]);
+console.log(cityButtonsParsed.length);
 function generateButtons(){
 for (i = 0; i<cityButtonsParsed.length; i++){
   $("#search-containers").append("<button class='citycontainer' id =" + i + "name:" + cityButtonsParsed[i] + ">" + cityButtonsParsed[i] + "</button>");
+if (cityButtonsParsed.length > 12){
+  $("#" + 12).remove();
+}
 }
 }
 generateButtons();
 
+var startqueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityButtonsParsed[0] + "&appid=5f20ec22761478f699827153c2dae50d";
+
+$.ajax({
+  url: startqueryURL,
+  method: "GET"
+})
+
+.then(function(response) {
+  console.log(response);
+          $("#chosen-city").text(response.name + " ");
+          var utc = moment.tz("Atlantic/Reykjavik");
+          var d = utc.add(response.timezone, 'seconds');
+          d = d.toString();
+          var cut = jQuery.trim(d).substring(0, 25)
+          .split(" ").slice(0, -1).join(" ");
+          $("#date").text(cut);
+          $("#temperature").text(Math.round(response.main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+          $("#humidity").text(response.main.humidity + "%");
+          $("#wind-speed").text(Math.round(response.wind.speed * 1.609) + " kph/" + Math.round(response.wind.speed) + " mph");
+          $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+          var lat = response.coord.lat;
+          var lon = response.coord.lon;
+
+      var startqueryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=5f20ec22761478f699827153c2dae50d&lat=" + lat + "&lon=" + lon;
+
+      $.ajax({
+        url: startqueryURL2,
+        method: "GET"
+      })
+
+      .then(function(response) {
+        console.log(response);
+        $("#uv-index").text(response.value);
+    });
+
+  });
+
+  var startqueryURL3 = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityButtonsParsed[0] + "&appid=5f20ec22761478f699827153c2dae50d";
+
+  $.ajax({
+    url: startqueryURL3,
+    method: "GET"
+  })
+
+  .then(function(response) {
+    console.log(response);
+ 
+    var local = moment.tz("Atlantic/Reykjavik").add(response.city.timezone, 'seconds');
+    var localdate1 = (moment(local, "DD-MM-YYYY").add(1, "days")).format('L');
+    var localdate2 = (moment(local, "DD-MM-YYYY").add(2, "days")).format('L');
+    var localdate3 = (moment(local, "DD-MM-YYYY").add(3, "days")).format('L');
+    var localdate4 = (moment(local, "DD-MM-YYYY").add(4, "days")).format('L');
+    var localdate5 = (moment(local, "DD-MM-YYYY").add(5, "days")).format('L');
+
+    $("#date1").text(localdate1);
+    $("#date2").text(localdate2);
+    $("#date3").text(localdate3);
+    $("#date4").text(localdate4);
+    $("#date5").text(localdate5);
+
+    $("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png");
+    $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png");
+    $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + "@2x.png");
+    $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + "@2x.png");
+    $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + "@2x.png");
+
+    $("#temp1").text("Temp: " + Math.round(response.list[8].main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.list[8].main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+    $("#temp2").text("Temp: " + Math.round(response.list[16].main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.list[16].main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+    $("#temp3").text("Temp: " + Math.round(response.list[24].main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.list[24].main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+    $("#temp4").text("Temp: " + Math.round(response.list[32].main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.list[32].main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+    $("#temp5").text("Temp: " + Math.round(response.list[39].main.temp - 273.15) + " " + "\xB0" + "C/" + Math.round(((response.list[39].main.temp - 273.15)* 9/5) + 32) + " " + "\xB0" + "F");
+
+    $("#humidity1").text(response.list[8].main.humidity + "%");
+    $("#humidity2").text(response.list[16].main.humidity + "%");
+    $("#humidity3").text(response.list[24].main.humidity + "%");
+    $("#humidity4").text(response.list[32].main.humidity + "%");
+    $("#humidity5").text(response.list[39].main.humidity + "%");
+  });
 /* var savedquery = localStorage.getItem("recent");
 document.getElementById("search-input").value = savedquery;
 var queryArray = localStorage.getItem("cityArray");
@@ -63,6 +145,7 @@ $("#search-button").on("click", function() {
       })
 
       .then(function(response) {
+        
 
 
      /*   if (cities < 12){
